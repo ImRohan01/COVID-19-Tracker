08c787849859119eceb7681dc1fefa7f75496fe3
ImRohan01 <rhshetty01@gmail.com>
2020-08-23 18:30:41 +0530

    Updates

---
+++ precache-manifest.075e3dd0cdefd87ac99fbdedc7c6ca14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941f7074204527835be79d2e17200097",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "c8565b3134703ce92554",
+    "url": "/COVID-19-Tracker/static/css/main.13ad78c1.chunk.css"
+  },
+  {
+    "revision": "bb0cd0973a5be305e3e8",
+    "url": "/COVID-19-Tracker/static/js/2.039e59ee.chunk.js"
+  },
+  {
+    "revision": "efe373883032cd282f4d2c1b869deab3",
+    "url": "/COVID-19-Tracker/static/js/2.039e59ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8565b3134703ce92554",
+    "url": "/COVID-19-Tracker/static/js/main.35c8b88d.chunk.js"
+  },
+  {
+    "revision": "8ee42f29da219c5ff15b",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.e9c1294c.js"
+  },
+  {
+    "revision": "127012906c3407d6b6140a1e89bd6ab7",
+    "url": "/COVID-19-Tracker/static/media/virus.12701290.png"
+  }
+]);
